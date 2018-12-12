ee79770867b9398f82443d652a5dc20981d720b5
fahadq123 <42976252+fahadq123@users.noreply.github.com>
2018-12-11 20:17:45 -0500

    Initial commit

---
+++ src/sudoku/sudoku_main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package sudoku;
+
+/**
+ * @author Fahad
+ *
+ */
+public class sudoku_main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
